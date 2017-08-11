47dd8d32395d05b2adae9d036bc57806a91dc1aa
Sowmya Logawamy <sowmyalogaswamy@gmail.com>
2017-08-11 13:10:26 -0700

    Add a dynamic segment and create a new route and a component to present each posts detail

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,14 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    update(post, params) {
-      this.sendAction('update', post, params);
-    },
-    delete(post) {
-      if(confirm('Are you sure you want to delete this question?')) {
-        this.sendAction('destroyPost', post);
-      }
-    }
-  }
+
 });
