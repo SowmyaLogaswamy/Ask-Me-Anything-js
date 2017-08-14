313104c593fdd13fda3a6b5fdc659b5bcb3d9357
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-08-14 16:48:39 -0700

    Add timestamp

--- app/models/comment.js
+++ app/models/comment.js
@@ -3,5 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   answer: DS.attr(),
+  created: DS.attr({
+    defaultValue(){
+    return new Date();
+    }
+  }),
   post: DS.belongsTo('post', { async: true })
 });
--- app/models/post.js
+++ app/models/post.js
@@ -4,5 +4,10 @@ export default DS.Model.extend({
   author: DS.attr(),
   question: DS.attr(),
   note: DS.attr(),
+  created: DS.attr({
+  defaultValue(){
+    return new Date();
+  }
+}),
   comments: DS.hasMany('comment', { async: true })
 });
